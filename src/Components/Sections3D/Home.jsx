44cc54328815f049b643a1519931a3063bb380d4
Nitesh-DK <nittesh.dk@gmail.com>
2025-01-02 21:29:54 +0530

    Refactor Home component by removing unused Leva controls and cleaning up imports for improved readability and performance.

--- src/Components/Sections3D/Home.jsx
+++ src/Components/Sections3D/Home.jsx
@@ -1,24 +1,6 @@
-import { useControls } from "leva";
-import { config } from "../../config";
 import { House2 } from "../3D/House2";
-import { Text } from "@react-three/drei";
 
 const Home = () => {
-  const textControls = useControls("Text Group", {
-    position: {
-      value: [-0.1, 2.14, -1.25],
-      step: 0.1,
-    },
-    rotation: {
-      value: [0, -0.65, 0],
-      step: 0.1,
-    },
-    scale: {
-      value: 1,
-      step: 0.1,
-    },
-  });
-
   return (
     <group>
       <House2 position={[-0.3, -0.1, 0]} rotation={[0, -0.65, 0]} scale={1} />
